8856d72ff01058a4cdd253f7451e467ed54fa57f
Vivian Martins <64610367+vivianmartins@users.noreply.github.com>
2022-04-28 23:05:27 +0100

    .

--- fx/src/main/java/com/example/fx/HelloApplication.java
+++ fx/src/main/java/com/example/fx/HelloApplication.java
@@ -2,18 +2,26 @@ package com.example.fx;
 
 import com.example.bd.BLL.ClienteBLL;
 import com.example.bd.BLL.PratoBLL;
+import com.example.bd.DAL.Pratos;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.List;
+
 import com.example.bd.DAL.Clientes;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        System.out.println(PratoBLL.readAll());
+        List<Pratos> listaPra = PratoBLL.readAll("Francesinha");
+        for(Pratos pra : listaPra)
+            System.out.println( " nome:  " + pra.getDescricao()  + " preço: "+ pra.getPrecoatual());
+
+
+
 
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
