9d82aa43069ae754213e4ce9ac4ba3b05389d96e
jcostaaaaa <100879576+jcostaaaaa@users.noreply.github.com>
2022-06-25 16:06:45 +0100

    h
    hj

--- webApp/src/main/java/com/example/webapp/teste.java
+++ webApp/src/main/java/com/example/webapp/teste.java
@@ -4,11 +4,9 @@ package com.example.webapp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import com.bd.BLL.EncomendaBLL;
 import com.bd.BLL.PratoBLL;
-import javax.persistence.*;
 import com.bd.DAL.Encomendas;
 import com.bd.DAL.Estafeta;
 import com.bd.DAL.Pratos;
@@ -18,9 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,20 +24,21 @@ import java.util.List;
 
 
 public class teste {
+    /*
+    @Autowired /* spring ejetar o DAO
+    private Pratos pratos;
+*/
 
     @GetMapping("listar")
-    public String listar (Model model){
-        List <Pratos> pratosList= PratoBLL.readAll(true);
-        model.addAttribute("Pratos", PratoBLL.readAll(true));
+            public String listar (Model model){
+            List <Pratos> pratosList= PratoBLL.readAll(true);
+            //  List<Pratos> listEnc = new ArrayList<Pratos>();
+            //listEnc.addAll(listEnc);
+            // model.addAttribute("Pratos", pratosList);
+   model.addAttribute("Pratos", PratoBLL.readAll(true));
 
-        return   "listar";
-    }
-
-    /***
-     * HOME
-     * @param model
-     * @return
-     */
+            return   "listar";
+        }
         @GetMapping("/")
         public String ViewHomePage(Model model){
 
